b14900c0ff90a249fe11bcacc4d454f79b254a30
Github Action <actions@github.com>
2021-10-15 16:37:23 +0000

    [Bot] Update lua source and docs

--- lua/fulib/init.lua
+++ lua/fulib/init.lua
@@ -37,7 +37,7 @@ M["not-list?"] = function(v)
   return not M["list?"](v)
 end
 local function flt_eq_3f(flt1, flt2)
-  return (math.abs((flt1 - flt2)) < 1e-06)
+  return (math.abs((flt1 - flt2)) < 1e-15)
 end
 local function subset_3f(tbl1, tbl2)
   local function _4_(v, k)
@@ -117,6 +117,12 @@ M["table-values"] = function(tbl)
   M.for_each(_11_, tbl)
   return ntbl
 end
+M.indexed = function(list)
+  local function _12_(_241, _242)
+    return {_242, _241}
+  end
+  return M.map(_12_, list)
+end
 M.append = function(tbl, v)
   tbl[(#tbl + 1)] = v
   return tbl
@@ -141,19 +147,19 @@ local function all_tbl(pred, tbl)
   return true
 end
 M.all = function(pred, tbl)
-  local _14_
+  local _15_
   if M["list?"](tbl) then
-    _14_ = all_lst
+    _15_ = all_lst
   else
-    _14_ = all_tbl
+    _15_ = all_tbl
   end
-  return _14_(pred, tbl)
+  return _15_(pred, tbl)
 end
 M.any = function(pred, tbl)
-  local function _16_(...)
+  local function _17_(...)
     return not pred(...)
   end
-  return not M.all(_16_, tbl)
+  return not M.all(_17_, tbl)
 end
 local function for_each_in_lst(f, lst)
   for i = 1, #lst do
@@ -168,89 +174,89 @@ local function for_each_in_tbl(f, tbl)
   return nil
 end
 M.for_each = function(f, tbl)
-  local _17_
+  local _18_
   if M["list?"](tbl) then
-    _17_ = for_each_in_lst
+    _18_ = for_each_in_lst
   else
-    _17_ = for_each_in_tbl
+    _18_ = for_each_in_tbl
   end
-  return _17_(f, tbl)
+  return _18_(f, tbl)
 end
 M.map = function(f, tbl)
   local ntbl = {}
   if M["list?"](tbl) then
-    local function _19_(_241, _242)
+    local function _20_(_241, _242)
       return M.append(ntbl, f(_241, _242))
     end
-    M.for_each(_19_, tbl)
+    M.for_each(_20_, tbl)
   else
-    local function _20_(_241, _242)
+    local function _21_(_241, _242)
       ntbl[_242] = f(_241, _242)
       return nil
     end
-    M.for_each(_20_, tbl)
+    M.for_each(_21_, tbl)
   end
   return ntbl
 end
 M.filter = function(pred, tbl)
-  local function _22_(_241, _242)
+  local function _23_(_241, _242)
     if pred(_241, _242) then
       return _241
     end
   end
-  return M.map(_22_, tbl)
+  return M.map(_23_, tbl)
 end
 M.foldl = function(f, init, lst)
   local acc = init
-  local function _24_(_241, _242)
+  local function _25_(_241, _242)
     acc = f(acc, _241, _242)
     return nil
   end
-  M.for_each(_24_, lst)
+  M.for_each(_25_, lst)
   return acc
 end
 M.foldr = function(f, init, lst)
   local acc = init
-  local function _25_(_241, _242)
+  local function _26_(_241, _242)
     acc = f(_241, acc, _242)
     return nil
   end
-  M.for_each(_25_, M.reverse(lst))
+  M.for_each(_26_, M.reverse(lst))
   return acc
 end
 M.reverse = function(lst)
-  local function _26_(_241, _242)
+  local function _27_(_241, _242)
     return lst[((#lst + 1) - _242)]
   end
-  return M.map(_26_, lst)
+  return M.map(_27_, lst)
 end
 local function to_exist_map(lst)
   local ntbl = {}
-  local function _27_(_241)
+  local function _28_(_241)
     ntbl[_241] = true
     return nil
   end
-  for_each_in_lst(_27_, lst)
+  for_each_in_lst(_28_, lst)
   return ntbl
 end
 local function intersect_lst(tbl1, tbl2)
   local in_tbl1 = to_exist_map(tbl1)
-  local function _28_(_241)
+  local function _29_(_241)
     return (in_tbl1)[_241]
   end
-  return M.filter(_28_, tbl2)
+  return M.filter(_29_, tbl2)
 end
 local function intersect_tbl(tbl1, tbl2)
   if M["list?"](tbl1) then
-    local function _29_(_241, _242)
+    local function _30_(_241, _242)
       return not M["nil?"]((tbl1)[_242])
     end
-    return M.filter(_29_, tbl2)
+    return M.filter(_30_, tbl2)
   else
-    local function _30_(_241, _242)
+    local function _31_(_241, _242)
       return not M["nil?"]((tbl2)[_242])
     end
-    return M.filter(_30_, tbl1)
+    return M.filter(_31_, tbl1)
   end
 end
 M.intersect = function(tbl1, tbl2)
@@ -269,15 +275,15 @@ local function range(start, _end)
 end
 M.zip_with = function(f, lst1, lst2)
   local len = math.min(#lst1, #lst2)
-  local function _33_(_241)
+  local function _34_(_241)
     return f((lst1)[_241], (lst2)[_241])
   end
-  return M.map(_33_, range(1, len))
+  return M.map(_34_, range(1, len))
 end
 M.zip = function(lst1, lst2)
-  local function _34_(_241, _242)
+  local function _35_(_241, _242)
     return {_241, _242}
   end
-  return M.zip_with(_34_, lst1, lst2)
+  return M.zip_with(_35_, lst1, lst2)
 end
 return M
